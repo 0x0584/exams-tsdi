7ca40a56e79161c64f71efa0542dd49bff374d2a
Anas Rchid <rchid.anas@gmail.com>
2017-05-29 03:26:59 +0000

    creating reports, this is :p

---
+++ EFF/2016/V1/D2 (30pts)/App/App/EtatListPartici.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class EtatListPartici: Form
+    {
+        public EtatListPartici()
+        {
+            InitializeComponent( );
+        }
+
+        private void EtatListPartici_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport( );
+        }
+    }
+}
