ad485d5a5beb6416131836971e889ece3190f579
Anas Rchid <rchid.anas@gmail.com>
2017-06-02 22:30:51 +0000

    add: half of Q4

--- EFF/2016/V3_3/D2 (30 pts)/App/App/SearchFormateur.Designer.cs
+++ EFF/2016/V3_3/D2 (30 pts)/App/App/SearchFormateur.Designer.cs
@@ -30,24 +30,24 @@
             this.textBox1 = new System.Windows.Forms.TextBox( );
             this.btnsearch = new System.Windows.Forms.Button( );
             this.dgv1 = new System.Windows.Forms.DataGridView( );
-            this.label3 = new System.Windows.Forms.Label( );
-            this.label2 = new System.Windows.Forms.Label( );
-            this.label1 = new System.Windows.Forms.Label( );
-            this.button2 = new System.Windows.Forms.Button( );
-            this.button1 = new System.Windows.Forms.Button( );
+            this.lbl_respo = new System.Windows.Forms.Label( );
+            this.lbl_nb_chapitres = new System.Windows.Forms.Label( );
+            this.lbl_ensei = new System.Windows.Forms.Label( );
+            this.btnetat = new System.Windows.Forms.Button( );
+            this.btnuvinfo = new System.Windows.Forms.Button( );
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit( );
             this.SuspendLayout( );
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(261, 58);
+            this.textBox1.Location = new System.Drawing.Point(93, 14);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
             this.textBox1.TabIndex = 0;
             // 
             // btnsearch
             // 
-            this.btnsearch.Location = new System.Drawing.Point(399, 56);
+            this.btnsearch.Location = new System.Drawing.Point(12, 14);
             this.btnsearch.Name = "btnsearch";
             this.btnsearch.Size = new System.Drawing.Size(75, 23);
             this.btnsearch.TabIndex = 1;
@@ -59,71 +59,73 @@
             // 
             this.dgv1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv1.Location = new System.Drawing.Point(21, 94);
+            this.dgv1.Location = new System.Drawing.Point(12, 43);
             this.dgv1.Name = "dgv1";
             this.dgv1.Size = new System.Drawing.Size(459, 236);
             this.dgv1.TabIndex = 2;
             // 
-            // label3
+            // lbl_respo
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(243, 23);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "label1";
+            this.lbl_respo.AutoSize = true;
+            this.lbl_respo.Location = new System.Drawing.Point(350, 303);
+            this.lbl_respo.Name = "lbl_respo";
+            this.lbl_respo.Size = new System.Drawing.Size(35, 13);
+            this.lbl_respo.TabIndex = 5;
+            this.lbl_respo.Text = "label1";
             // 
-            // label2
+            // lbl_nb_chapitres
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(148, 23);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "label1";
+            this.lbl_nb_chapitres.AutoSize = true;
+            this.lbl_nb_chapitres.Location = new System.Drawing.Point(436, 291);
+            this.lbl_nb_chapitres.Name = "lbl_nb_chapitres";
+            this.lbl_nb_chapitres.Size = new System.Drawing.Size(35, 13);
+            this.lbl_nb_chapitres.TabIndex = 6;
+            this.lbl_nb_chapitres.Text = "label1";
             // 
-            // label1
+            // lbl_ensei
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(189, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            this.lbl_ensei.AutoSize = true;
+            this.lbl_ensei.Location = new System.Drawing.Point(350, 290);
+            this.lbl_ensei.Name = "lbl_ensei";
+            this.lbl_ensei.Size = new System.Drawing.Size(35, 13);
+            this.lbl_ensei.TabIndex = 7;
+            this.lbl_ensei.Text = "label1";
             // 
-            // button2
+            // btnetat
             // 
-            this.button2.Location = new System.Drawing.Point(12, 42);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnetat.Location = new System.Drawing.Point(216, 14);
+            this.btnetat.Name = "btnetat";
+            this.btnetat.Size = new System.Drawing.Size(132, 23);
+            this.btnetat.TabIndex = 3;
+            this.btnetat.Text = "Etat List des chapitres";
+            this.btnetat.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnuvinfo
             // 
-            this.button1.Location = new System.Drawing.Point(12, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnuvinfo.Location = new System.Drawing.Point(354, 14);
+            this.btnuvinfo.Name = "btnuvinfo";
+            this.btnuvinfo.Size = new System.Drawing.Size(117, 23);
+            this.btnuvinfo.TabIndex = 4;
+            this.btnuvinfo.Text = "UV Ensei/Respo";
+            this.btnuvinfo.UseVisualStyleBackColor = true;
+            this.btnuvinfo.Click += new System.EventHandler(this.btnuvinfo_Click);
             // 
             // SearchFormateur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(509, 347);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(486, 328);
+            this.Controls.Add(this.lbl_respo);
+            this.Controls.Add(this.lbl_nb_chapitres);
+            this.Controls.Add(this.lbl_ensei);
+            this.Controls.Add(this.btnetat);
+            this.Controls.Add(this.btnuvinfo);
             this.Controls.Add(this.dgv1);
             this.Controls.Add(this.btnsearch);
             this.Controls.Add(this.textBox1);
             this.Name = "SearchFormateur";
             this.Text = "SearchFormateur";
+            this.Load += new System.EventHandler(this.SearchFormateur_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit( );
             this.ResumeLayout(false);
             this.PerformLayout( );
@@ -135,10 +137,10 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnsearch;
         private System.Windows.Forms.DataGridView dgv1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_respo;
+        private System.Windows.Forms.Label lbl_nb_chapitres;
+        private System.Windows.Forms.Label lbl_ensei;
+        private System.Windows.Forms.Button btnetat;
+        private System.Windows.Forms.Button btnuvinfo;
     }
 }
