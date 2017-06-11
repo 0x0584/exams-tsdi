6e99e94ae5a3bfd658def099bb4acffc36cbf990
Anas Rchid <rchid.anas@gmail.com>
2017-06-11 17:57:40 +0000

    FINISH 2016 Day 3 V3. I HATE YOU OO!
    Signed-off-by: Anas Rchid <rchid.anas@gmail.com>

---
+++ EFF/2016/V3_3/D3 (26 pts)/SiteWeb/SiteWeb/ServiceUV.asmx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace SiteWeb
+{
+    /// <summary>
+    /// Description résumée de ServiceUV
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Pour autoriser l'appel de ce service Web depuis un script à l'aide d'ASP.NET AJAX, supprimez les marques de commentaire de la ligne suivante. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServiceUV: System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public string InfoFormation(int numF)
+        {
+            string str = "";
+
+            using (var cmd = new System.Data.SqlClient.SqlCommand( )) {
+                cmd.Connection = new System.Data.SqlClient.SqlConnection( );
+                cmd.Connection.ConnectionString = "Server = WINXP\\SQLEXPRESS;" +
+                                                  "Initial Catalog = ff2016_v33;" +
+                                                  "Integrated Security = TRUE;";
+                cmd.Connection.Open( );
+                cmd.CommandText = "info";
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@idf", numF);
+                using (var reader = cmd.ExecuteReader( )) {
+                    reader.Read( );
+                    str = string.Format("NB UV: {0} | F PERM: {1} | F VACA {2} ",
+                                                reader["nombreUV"].ToString( ),
+                                                reader["nb_per"].ToString( ),
+                                                reader["nb_vac"].ToString( ));
+                    reader.Close( );
+                }
+                cmd.Connection.Close( );
+            }
+
+            return str;
+        }
+    }
+}
---
+++ EFF/2016/V3_3/D3 (26 pts)/SiteWeb/SiteWeb/TestService.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace SiteWeb {
+    
+    
+    public partial class TestService {
+        
+        /// <summary>
+        /// Contrôle form1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Contrôle DropDownList1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// Contrôle Button1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Contrôle Label1.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+    }
+}
