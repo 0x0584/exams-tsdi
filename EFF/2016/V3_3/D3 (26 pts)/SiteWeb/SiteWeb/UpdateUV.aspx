2d80c0366b41f4cbc5f0526a7ad31e375e48002b
Anas Rchid <rchid.anas@gmail.com>
2017-06-11 05:11:52 +0000

    starting Q5

---
+++ EFF/2016/V3_3/D3 (26 pts)/SiteWeb/SiteWeb/UpdateUV.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UpdateUV.aspx.cs" Inherits="SiteWeb.UpdateUV" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ListBox ID="listUV" runat="server" Width="262px"></asp:ListBox>
+        <asp:Button Style="margin:0px 0px 30px 10px;" ID="btncheck" runat="server" 
+            Text="Select an UV" onclick="btncheck_Click" /><br />
+        <asp:Button ID="btndel" runat="server" Text="Delete UV" OnClick="btndel_Click" />
+        <asp:Button ID="btnupdate" runat="server" Text="Update UV" />
+        <asp:Button ID="btnselected" runat="server" Text="Show Chapiters" />
+        <asp:Button ID="btnfilter" runat="server" Text="Filtering" />
+    </div>
+    <br />
+    <br />
+    <div>
+        <br />
+        <asp:Label ID="lblinfo" runat="server" Text="Label"></asp:Label>
+        <br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ EFF/2016/V3_3/D3 (26 pts)/SiteWeb/SiteWeb/UpdateUV.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace SiteWeb
+{
+    public partial class UpdateUV: System.Web.UI.Page
+    {
+        SqlCommand commander = new SqlCommand( );
+        SqlDataReader reader = null;
+        int numf = 0;
+        string currentUV = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            commander.Connection = new SqlConnection("Server = WINXP\\SQLEXPRESS;" +
+                                                     "Initial Catalog = ff2016_v33;" +
+                                                     "Integrated Security = TRUE;");
+            Session["Formation"] = 1;
+
+            if (Session["Formation"] == null) Response.Redirect("~/Default.aspx");
+            else numf = int.Parse(Session["Formation"].ToString( ));
+
+            if (IsPostBack) return; // skip the rest..
+
+            btndel.Enabled = btnfilter.Enabled = false;
+            btnselected.Enabled = btnupdate.Enabled = false;
+
+            #region Fill ListUV
+            commander.Connection.Open( );
+            commander.CommandText = "SELECT * FROM UV";
+            reader = commander.ExecuteReader( );
+
+            List<string> numUVs = new List<string>( );
+
+            while (reader.Read( )) {
+                string item = string.Format("{0} [{1}hrs]", reader["nomUV"].ToString( ),
+                                                            reader["massHoraire"].ToString( ));
+                listUV.Items.Add(item);
+                numUVs.Add(reader["numUV"].ToString( ));
+            }
+            reader.Close( );
+
+            for (int i = 0; i < listUV.Items.Count; i++) {
+                listUV.Items[i].Value = numUVs[i];
+            }
+
+            commander.Connection.Close( );
+            #endregion
+        }
+
+        protected void btndel_Click(object sender, EventArgs e)
+        {
+            // TODO: find how to confirm!
+            // 
+
+
+        }
+
+        protected void btncheck_Click(object sender, EventArgs e)
+        {
+            commander.Connection.Open( );
+            commander.CommandText = "SELECT * FROM UV WHERE numUV = @n";
+            commander.Parameters.AddWithValue("@n", int.Parse(listUV.SelectedItem.Value));
+            
+            reader = commander.ExecuteReader( );
+            reader.Read( );
+            lblinfo.Text = string.Format("NUM: {0} | NOM: {1} | MASS HORAIRE: {2}",
+                                            reader["numUV"].ToString( ),
+                                            reader["nomUV"].ToString( ),
+                                            reader["massHoraire"].ToString( ));
+            reader.Close( );
+            commander.Connection.Close( );
+            
+            btndel.Enabled = btnfilter.Enabled = true;
+            btnselected.Enabled = btnupdate.Enabled = true;
+        }
+    }
+}
